56b2ef96b2e33c027a93a2cc7b881de725e193fd
Lucas Wagner <lowagner@gmail.com>
2025-09-16 06:12:40 -0700

    add some logging

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
+
+    let mut arg_count = 0;
+    let mut paths: Vec<String> = vec![];
+
+    for path in std::env::args() {
+        if arg_count > 0 {
+            paths.push(path);
+        }
+        arg_count += 1;
+    }
+
+    std::assert!(arg_count > 1, "usage: `fthis [file_path] [directory_path]`");
+
+    for path in paths {
+        log::warn!("(not actually) watching {path}");
+    }
 }
