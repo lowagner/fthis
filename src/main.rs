f87da0c4e4fa99a1ebe0a5c43a8020efe5d44ba9
Lucas Wagner <lowagner@gmail.com>
2025-09-16 17:01:30 -0700

    use OsString for file paths

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use std::ffi::OsStr;
+use std::ffi::{OsStr, OsString};
 
 use inotify::{Event, Inotify, WatchDescriptor, WatchMask};
 
@@ -7,11 +7,11 @@ fn main() {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
 
     let mut arg_count = 0;
-    let mut paths: Vec<String> = vec![];
+    let mut paths: Vec<OsString> = vec![];
 
     for path in std::env::args() {
         if arg_count > 0 {
-            paths.push(path);
+            paths.push(path.into());
         }
         arg_count += 1;
     }
@@ -32,7 +32,7 @@ fn main() {
 
 struct Watcher {
     inotify: Inotify,
-    watch_map: HashMap<WatchDescriptor, String>,
+    watch_map: HashMap<WatchDescriptor, OsString>,
 }
 
 impl Watcher {
@@ -43,15 +43,19 @@ impl Watcher {
         }
     }
 
-    fn add(&mut self, path: String) -> Result<(), std::io::Error> {
-        match self.inotify.watches().add(path.as_str(), WatchMask::MODIFY) {
+    fn add(&mut self, path: OsString) -> Result<(), std::io::Error> {
+        match self
+            .inotify
+            .watches()
+            .add(path.as_os_str(), WatchMask::MODIFY)
+        {
             Ok(watch) => {
-                log::info!("watching {path}");
+                log::info!("watching {path:?}");
                 self.watch_map.insert(watch, path);
                 Ok(())
             }
             Err(err) => {
-                log::warn!("could not watch {path}, error: {err}");
+                log::warn!("could not watch {path:?}, error: {err}");
                 Err(err)
             }
         }
@@ -65,18 +69,18 @@ impl Watcher {
             .expect("error while reading events");
 
         for event in events {
-            let file_path = self.event_name(&event);
-            log::info!("got event from file {file_path}");
+            let file_path = self.file_path(&event);
+            log::info!("got event from file {file_path:?}");
         }
     }
 
-    fn event_name(&self, event: &Event<&OsStr>) -> String {
+    fn file_path(&self, event: &Event<&OsStr>) -> OsString {
         if let Some(name) = event.name {
-            return name.to_os_string().into_string().expect("ok");
+            return name.to_os_string();
         }
         if let Some(name) = self.watch_map.get(&event.wd) {
             return name.clone();
         }
-        return "?".into();
+        return "".into();
     }
 }
