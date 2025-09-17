ef755f5f67463b9da0938de0dfe478b9576bfed0
Lucas Wagner <lowagner@gmail.com>
2025-09-16 20:33:01 -0700

    add bash child process

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
+use core::fmt::{self, Write};
 use std::collections::HashMap;
 use std::ffi::{OsStr, OsString};
+use std::io::{self, Read as IoRead, Write as IoWrite};
+use std::process::{self, Stdio};
 
 use inotify::{Event, Inotify, WatchDescriptor, WatchMask};
 
@@ -15,9 +18,10 @@ fn main() {
         }
         arg_count += 1;
     }
+
     std::assert!(arg_count > 1, "usage: `fthis [file_path] [directory_path]`");
 
-    let mut watcher = Watcher::init();
+    let mut watcher = Watcher::new("bash");
 
     let mut success_count = 0;
     for path in paths {
@@ -33,14 +37,69 @@ fn main() {
     }
 }
 
+struct Buffer<const N: usize> {
+    buffer: [u8; N],
+    cursor: usize,
+}
+
+impl<const N: usize> Buffer<N> {
+    fn new() -> Self {
+        Self {
+            buffer: [0; N],
+            cursor: 0,
+        }
+    }
+
+    fn pull(&mut self) -> &[u8] {
+        let length = self.cursor;
+        self.cursor = 0;
+        &self.buffer[0..length]
+    }
+
+    fn read_from<R>(&mut self, readable: &mut R) -> io::Result<()>
+    where
+        R: IoRead,
+    {
+        let bytes = readable.read(&mut self.buffer[self.cursor..])?;
+        self.cursor += bytes;
+        Ok(())
+    }
+}
+
+impl<const N: usize> Write for Buffer<N> {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        if self.cursor + s.len() <= N {
+            let next_cursor = self.cursor + s.len();
+            self.buffer[self.cursor..next_cursor].copy_from_slice(s.as_bytes());
+            self.cursor = next_cursor;
+            Ok(())
+        } else {
+            Err(fmt::Error)
+        }
+    }
+}
+
 struct Watcher {
+    child_process: process::Child,
     inotify: Inotify,
     watch_map: HashMap<WatchDescriptor, OsString>,
 }
 
+impl Drop for Watcher {
+    fn drop(&mut self) {
+        let _ = self.child_process.kill();
+    }
+}
+
 impl Watcher {
-    fn init() -> Self {
+    fn new(shell: &str) -> Self {
         Self {
+            child_process: process::Command::new(shell)
+                // Don't share stdin/out with this (parent) process:
+                .stdin(Stdio::piped())
+                .stdout(Stdio::piped())
+                .spawn()
+                .expect("failed to spawn a child shell process"),
             inotify: Inotify::init().expect("error initializing inotify instance"),
             watch_map: HashMap::new(),
         }
@@ -97,7 +156,33 @@ impl Watcher {
             return;
         }
         log::info!("got update from file {file_path:?}");
+        self.execute(file_path.as_os_str());
         // NOTE: for some reason, individual files need to be re-watched.
+        // this works out for us because we don't want `fthis`'s writes
+        // (via `self.execute`) to trigger another update before the user
+        // makes a change.
         let _ = self.add(file_path);
+    }
+
+    fn execute(&mut self, file_path: &OsStr) {
+        let mut buffer = Buffer::<1024>::new();
+        let _ = std::write!(&mut buffer, "echo asdf\n");
+        self.child_process
+            .stdin
+            .as_mut()
+            .unwrap()
+            .write_all(buffer.pull())
+            .expect("couldn't write to child process stdin");
+        self.child_process
+            .stdin
+            .as_mut()
+            .unwrap()
+            .flush()
+            .expect("couldn't flush child process stdin");
+        buffer
+            .read_from(self.child_process.stdout.as_mut().unwrap())
+            .expect("couldn't read child process stdout");
+        let output = std::str::from_utf8(buffer.pull()).expect("ok");
+        log::info!("got output {output}");
     }
 }
